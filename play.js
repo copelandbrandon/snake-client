4a0e982fca5bc6a9a9cc6b95094dd782affdd982
Brandon Copeland <copelandbrandon2@gmail.com>
2021-05-05 19:17:35 +0000

    separated client connection into separate module and imported into play

--- play.js
+++ play.js
@@ -1,20 +1,4 @@
 const net = require('net');
+const connect = require('./client').connect;
 
-//establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "135.23.222.131",
-    port: 50542
-  });
-  
-  //interpret incoming data as text
-  conn.setEncoding('utf8');
-  
-  conn.on('data', (message)=>{
-    console.log('Server sent:', message);
-  });
-  return conn;
-};
-
-console.log('Connecting ...');
 connect();
