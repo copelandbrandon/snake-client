b4c625f89e8382ee0603e3d1970dc91c8ccf7011
Brandon Copeland <copelandbrandon2@gmail.com>
2021-05-05 19:44:20 +0000

    refactored input functions into their own module and exported to play.js

--- play.js
+++ play.js
@@ -1,24 +1,6 @@
 const net = require('net');
-const connect = require('./client').connect;
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    console.log('Connection terminated by user');
-    process.exit();
-  }
-};
-
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-};
-
+const {connect} = require('./client');
+const {setupInput} = require('./input');
 
 setupInput();
 connect();
