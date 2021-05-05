60de374f9efb907fdc02013acb3436450b8e57f8
Brandon Copeland <copelandbrandon2@gmail.com>
2021-05-05 20:27:04 +0000

    passed connect object to input in order to allow for movement commands to be sent to server and implemented movement

--- input.js
+++ input.js
@@ -1,15 +1,9 @@
 const net = require('net');
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    console.log('Connection terminated by user');
-    process.exit();
-  }
-};
+let connection;
 
 // setup interface to handle user input from stdin
-
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -17,6 +11,23 @@ const setupInput = function () {
   stdin.on('data', handleUserInput);
   return stdin;
 };
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    console.log('Connection terminated by user');
+    process.exit();
+  } else if (key === 'w') {
+    connection.write('Move: up');
+  } else if (key === 'a') {
+    connection.write('Move: left');
+  } else if (key === 's') {
+    connection.write('Move: down');
+  } else if(key === 'd') {
+    connection.write('Move: right');
+  }
+};
+
+
 
 module.exports = {
   setupInput
--- play.js
+++ play.js
@@ -2,5 +2,6 @@ const net = require('net');
 const {connect} = require('./client');
 const {setupInput} = require('./input');
 
-setupInput();
-connect();
+
+setupInput(connect());
+
